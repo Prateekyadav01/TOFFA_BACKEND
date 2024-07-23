cf2d4f6dda87df15d1e6d53e26473cbaa1f40197
PrateekTechmatys <prateeky562@gmail.com>
2024-07-23 10:41:38 +0530

    User Model is done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
 const app = express();
+
+
+
+//in build middleWare are
+app.use(express.json());
 
 
 export{app}
--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -10,5 +11,48 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    
+    DOB:{
+        type: String,
+        required:[true, "DOb is required"]
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
 })
+
+userSchema.pre('save', function(){
+    const user = this;
+    if(user.isModified('password')){
+        user.password = bcrypt.hashSync(user.password, 10);
+    }
+})
+
+userSchema.methods.GENERATEACCESSTOKEN = function(){
+    try {
+        return jwt.sign(
+            {
+                _id:this._id,
+                email:this.email
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: '1h',
+            }
+        )
+    } catch (error) {
+        console.log("error in access token",error);
+    }
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
